7e35654e1e6e8ed235b1b189bad5e1cf1750b122
Tasman Powis <apowis@princeton.edu>
2023-02-14 08:37:54 -0500

    Annotated main.c

--- main.c
+++ main.c
@@ -9,13 +9,14 @@ int main(int argc, char **argv)
     int i, num = 100;
     double *dat;
 
+    //Allocating the array space
     dat = (double *) malloc(num * sizeof(double));
 
+    //Initializing the array
     for (i = 0; i < num; i++)
     {
         dat[i] = (double) i;
     }
-
 
 
 
